10a3c9e48e3b16958489aa836dcf6dae91671273
SamukaAlves000 <60469274+SamukaAlves000@users.noreply.github.com>
2020-07-02 07:58:40 -0300

    readById e update Product
    readById e update Product e adição dos botões de ação edit e delete

--- frontend/src/app/components/product/product.service.ts
+++ frontend/src/app/components/product/product.service.ts
@@ -30,4 +30,14 @@ export class ProductService {
   read(): Observable<Product []> {
    return this.http.get<Product []>(this.baseUrl)
   }
+
+  readById(id:string): Observable<Product> {
+    const url = `${this.baseUrl}/${id}`
+    return this.http.get<Product>(url)
+   }
+
+   update(produtc:Product):Observable<Product>{
+    const url = `${this.baseUrl}/${produtc.id}`
+    return this.http.put<Product>(url,produtc)
+  }
 }
