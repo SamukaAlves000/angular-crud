a9bd2e469078029a1c99ad134a04c7f529442ed3
SamukaAlves000 <60469274+SamukaAlves000@users.noreply.github.com>
2020-07-02 18:37:25 -0300

    Exibindo as Mensagens de Erro
    Exibindo as Mensagens de Erro

--- frontend/src/app/components/product/product.service.ts
+++ frontend/src/app/components/product/product.service.ts
@@ -2,7 +2,8 @@ import { Product } from './product.model';
 import { Injectable } from '@angular/core';
 import {MatSnackBar} from '@angular/material/snack-bar';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, EMPTY } from 'rxjs';
+import { catchError, map } from 'rxjs/operators';
 
 
 @Injectable({
@@ -13,18 +14,22 @@ export class ProductService {
   baseUrl = "http://localhost:3001/products"
   constructor(private snackBar: MatSnackBar,private http: HttpClient) { }
 
-  showMessage(msg: string):void{
+  showMessage(msg: string, isError: boolean = false):void{
       this.snackBar.open(msg,'X',
       {
         duration:3000,
         horizontalPosition:"right",
-        verticalPosition: "top"
+        verticalPosition: "top",
+        panelClass: isError ? ['msg-error'] : ['msg-sucess']
       }
       )
   }
 
   create(produtc:Product):Observable<Product>{
-    return this.http.post<Product>(this.baseUrl,produtc)
+    return this.http.post<Product>(this.baseUrl,produtc).pipe(
+      map((obj) => obj),
+      catchError((e) =>this.errorHandler(e))
+    );
   }
 
   read(): Observable<Product []> {
@@ -38,11 +43,22 @@ export class ProductService {
 
    update(produtc:Product):Observable<Product>{
     const url = `${this.baseUrl}/${produtc.id}`
-    return this.http.put<Product>(url,produtc)
+    return this.http.put<Product>(url,produtc).pipe(
+      map((obj) => obj),
+      catchError((e) =>this.errorHandler(e))
+    );
   }
 
   delete(id:number):Observable<Product>{
     const url = `${this.baseUrl}/${id}`
-    return this.http.delete<Product>(url)
+    return this.http.delete<Product>(url).pipe(
+      map((obj) => obj),
+      catchError((e) =>this.errorHandler(e))
+    );
+  }
+
+  errorHandler(e:any) : Observable<any>{
+    this.showMessage('Ocorreu um erro!',true)
+    return EMPTY
   }
 }
